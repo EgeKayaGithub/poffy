16aa68a43e09a6e49d4b8d8dbe6dbcbe972e152e
sanalsozluk <wolfappsoffical@gmail.com>
2024-07-15 23:02:54 +0300

    Site is building

---
+++ JS Files/navbar.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('navbar.html')
+        .then(response => response.text())
+        .then(data => {
+            const navbar = document.createElement('div');
+            navbar.innerHTML = data;
+            document.body.insertBefore(navbar, document.body.firstChild);
+        });
+});
+
+
+function showDisplayOfTheHamburgerMenu(){
+    let hamburgerMenuContainer = document.querySelectorAll(".hamburgerMenuContainer")
+
+    for(let i = 0; i<=hamburgerMenuContainer.length; i++ ){
+        if(hamburgerMenuContainer[i].style.width == "100vw"){
+            hamburgerMenuContainer[i].style.width = "0vw"
+        }
+        else{
+            hamburgerMenuContainer[i].style.width = "100vw"
+        }
+    }
+}
